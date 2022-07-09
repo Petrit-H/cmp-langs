ff9a35dd7dcbc84863424b5aa6f0f4d159dd197b
Petrit Halabaku <petrithalabaku@Petrits-MacBook-Pro.local>
2022-07-09 07:55:29 +0200

    v.1.0.3

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// const English = "./en.json";
+// const Czech = "./cs.json";
+// import cs from "./langs/cs.json";
+// import en from "./langs/en.json";
+
+import English from "./langs/en.json" assert { type: "json" };
+import Czech from "./langs/cs.json" assert { type: "json" };
+
+
+export const data = [English, Czech];
