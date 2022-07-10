6f15e2435dd4860db92afa97fd2ce36aa4b12532
Petrit Halabaku <petrithalabaku@Petrits-MacBook-Pro.local>
2022-07-10 23:46:46 +0200

    v-1.0.5

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@
 // import cs from "./langs/cs.json";
 // import en from "./langs/en.json";
 
-import English from "./langs/en.json" assert { type: "json" };
-import Czech from "./langs/cs.json" assert { type: "json" };
+import English from "./langs/en.json" ;
+import Czech from "./langs/cs.json" ;
 
-// console.log("🚀 ~ ", English,Czech)
+console.log("🚀 ~ ", `English`,English,`Czech`,Czech);
 
 export const data = [English, Czech];
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.js",
+  },
+  module: {
+    rules: [
+      {
+        type: "javascript/auto",
+        test: /\.json$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "langs/[name].[ext]",
+            },
+          },
+        ],
+        include: [path.resolve(__dirname, "src/langs")],
+      },
+      {
+        test: /\.(js|jsx)$/,
+        loader: "babel-loader",
+        exclude: /[\\/]node_modules[\\/]/,
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      inject: "body",
+      template: path.resolve(__dirname, "./src", "index.html"),
+    }),
+    new CleanWebpackPlugin(),
+  ],
+};
+
+// module.exports = config;
